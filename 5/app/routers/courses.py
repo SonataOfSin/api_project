29320557005b45fb95073fe6162a1fd9dccf56f6
SonataOfSin <n.chigladze112@gmail.com>
2026-09-17 23:43:00 +0400

    Delete 5/app directory

--- 5/app/routers/courses.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix="/courses",
-    tags=["Courses"]
-)
-
-# ენდპოინტები დაემატება მოგვიანებით
--- 5/app/routers/students.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix="/students",
-    tags=["Students"]
-)
-
-# ენდპოინტები დაემატება მოგვიანებით
